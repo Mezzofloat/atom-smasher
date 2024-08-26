0be11926cd4ed59c1860adef015348af8272ab9f
Mezzofloat <skull.wizard.2299@gmail.com>
2024-08-26 15:37:02 -0400

    changed energylevel to use integers instead of an enum

--- Assets/Scripts/ChangeColorWithEnergy.cs
+++ Assets/Scripts/ChangeColorWithEnergy.cs
@@ -4,42 +4,45 @@ using UnityEngine;
 
 public class ChangeColorWithEnergy : MonoBehaviour
 {
-    public EnergyLevel energyLevel;
+    [SerializeField] List<Color> energyColors;
+    [SerializeField] int maxEnergyLevel;
+
+    int energyLevel;
 
     public void DecrementEnergyLevel(int i) {
         if (i >= (int)energyLevel) Destroy(gameObject);
 
-        SetEnergyLevel((EnergyLevel)((int)energyLevel - i));
+        SetEnergyLevel(energyLevel - i);
     }
 
-    public void SetEnergyLevel(EnergyLevel e) {
+    public void SetEnergyLevel(int e) {
         switch (e) {
-            case EnergyLevel.Red:
+            case 0:
                 GetComponent<SpriteRenderer>().color = Color.red;
                 transform.GetChild(0).GetComponent<TrailRenderer>().endColor = transform.GetChild(0).GetComponent<TrailRenderer>().startColor = Color.red;
                 GetComponent<MovePhoton>().photonSpeed = 10;
                 break;
-            case EnergyLevel.Orange:
+            case 1:
                 GetComponent<SpriteRenderer>().color = Color.red + Color.yellow;
                 transform.GetChild(0).GetComponent<TrailRenderer>().endColor = transform.GetChild(0).GetComponent<TrailRenderer>().startColor = Color.red + Color.yellow;
                 GetComponent<MovePhoton>().photonSpeed = 15;
                 break;
-            case EnergyLevel.Yellow:
+            case 2:
                 GetComponent<SpriteRenderer>().color = Color.yellow;
                 transform.GetChild(0).GetComponent<TrailRenderer>().endColor = transform.GetChild(0).GetComponent<TrailRenderer>().startColor = Color.yellow;
                 GetComponent<MovePhoton>().photonSpeed = 20;
                 break;
-            case EnergyLevel.Green:
+            case 3:
                 GetComponent<SpriteRenderer>().color = Color.green;
                 transform.GetChild(0).GetComponent<TrailRenderer>().endColor = transform.GetChild(0).GetComponent<TrailRenderer>().startColor = Color.green;
                 GetComponent<MovePhoton>().photonSpeed = 25;
                 break;
-            case EnergyLevel.Blue:
+            case 4:
                 GetComponent<SpriteRenderer>().color = Color.blue;
                 transform.GetChild(0).GetComponent<TrailRenderer>().endColor = transform.GetChild(0).GetComponent<TrailRenderer>().startColor = Color.blue;
                 GetComponent<MovePhoton>().photonSpeed = 30;
                 break;
-            case EnergyLevel.Purple:
+            case 5:
                 GetComponent<SpriteRenderer>().color = Color.magenta;
                 transform.GetChild(0).GetComponent<TrailRenderer>().endColor = transform.GetChild(0).GetComponent<TrailRenderer>().startColor = Color.white;
                 GetComponent<MovePhoton>().photonSpeed = 35;
@@ -56,13 +59,4 @@ public class ChangeColorWithEnergy : MonoBehaviour
         SetEnergyLevel(energyLevel);
     }
 
-}
-
-public enum EnergyLevel {
-    Red = 1,
-    Orange = 2,
-    Yellow = 3,
-    Green = 4,
-    Blue = 5,
-    Purple = 6
 }
