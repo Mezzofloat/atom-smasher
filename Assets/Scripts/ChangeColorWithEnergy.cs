0df51d0081bc6dde70b0918b8fdf0f45dd419b28
Mezzofloat <skull.wizard.2299@gmail.com>
2024-08-26 15:40:00 -0400

    factored out the color changes

--- Assets/Scripts/ChangeColorWithEnergy.cs
+++ Assets/Scripts/ChangeColorWithEnergy.cs
@@ -6,6 +6,7 @@ public class ChangeColorWithEnergy : MonoBehaviour
 {
     [SerializeField] List<Color> energyColors;
     [SerializeField] int maxEnergyLevel;
+    [SerializeField] TrailRenderer tr;
 
     int energyLevel;
 
@@ -16,35 +17,28 @@ public class ChangeColorWithEnergy : MonoBehaviour
     }
 
     public void SetEnergyLevel(int e) {
+        var c = energyColors[e];
+
+        GetComponent<SpriteRenderer>().color = c;
+        tr.startColor = tr.endColor = c;
+
         switch (e) {
             case 0:
-                GetComponent<SpriteRenderer>().color = Color.red;
-                transform.GetChild(0).GetComponent<TrailRenderer>().endColor = transform.GetChild(0).GetComponent<TrailRenderer>().startColor = Color.red;
                 GetComponent<MovePhoton>().photonSpeed = 10;
                 break;
             case 1:
-                GetComponent<SpriteRenderer>().color = Color.red + Color.yellow;
-                transform.GetChild(0).GetComponent<TrailRenderer>().endColor = transform.GetChild(0).GetComponent<TrailRenderer>().startColor = Color.red + Color.yellow;
                 GetComponent<MovePhoton>().photonSpeed = 15;
                 break;
             case 2:
-                GetComponent<SpriteRenderer>().color = Color.yellow;
-                transform.GetChild(0).GetComponent<TrailRenderer>().endColor = transform.GetChild(0).GetComponent<TrailRenderer>().startColor = Color.yellow;
                 GetComponent<MovePhoton>().photonSpeed = 20;
                 break;
             case 3:
-                GetComponent<SpriteRenderer>().color = Color.green;
-                transform.GetChild(0).GetComponent<TrailRenderer>().endColor = transform.GetChild(0).GetComponent<TrailRenderer>().startColor = Color.green;
                 GetComponent<MovePhoton>().photonSpeed = 25;
                 break;
             case 4:
-                GetComponent<SpriteRenderer>().color = Color.blue;
-                transform.GetChild(0).GetComponent<TrailRenderer>().endColor = transform.GetChild(0).GetComponent<TrailRenderer>().startColor = Color.blue;
                 GetComponent<MovePhoton>().photonSpeed = 30;
                 break;
             case 5:
-                GetComponent<SpriteRenderer>().color = Color.magenta;
-                transform.GetChild(0).GetComponent<TrailRenderer>().endColor = transform.GetChild(0).GetComponent<TrailRenderer>().startColor = Color.white;
                 GetComponent<MovePhoton>().photonSpeed = 35;
                 break;
             default:
