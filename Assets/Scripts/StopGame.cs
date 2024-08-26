5512acb25b56006ab425d8bf71c798f1fb3f8db7
Mezzofloat <skull.wizard.2299@gmail.com>
2024-08-26 18:48:07 -0400

    time now slows to a stop instead of stopping altogether

--- Assets/Scripts/StopGame.cs
+++ Assets/Scripts/StopGame.cs
@@ -7,10 +7,15 @@ using DG.Tweening;
 public class StopGame : MonoBehaviour
 {
     [SerializeField] GameObject gameOverScreen;
+    [SerializeField] MoveNucleusWithMouse mNWM; 
 
     public void OnStop() {
-        Time.timeScale = 0;
-        gameOverScreen.SetActive(true);
+        mNWM.shouldMove = false;
+        DOVirtual.Float(1, 0, 2, (float f) => {
+            Time.timeScale = f;
+        }).SetUpdate(true).OnComplete(() => {
+            gameOverScreen.SetActive(true);
+        }).SetEase(Ease.OutSine);
     }
 
     public void Restart() {
